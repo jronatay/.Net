1d478d6e61e6c381a415dbb323a083bfa84f68d9
jronatay <jronatay25@gmail.com>
2015-01-28 18:10:54 +0800

    OrderRequest

---
+++ OrderRequestWeb/EntityLibrary/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EntityLibrary
+{
+    public class CustomerRepository
+    {
+        private OrderRequestEntities database = new OrderRequestEntities();
+
+        public void Save(Customer customer)
+        {
+            database.Customers.Add(customer);
+            database.SaveChanges();
+        }
+
+        public Boolean IsEmailExist(string EmailAddress)
+        {
+            var result = database.Customers.Where(customer => customer.EmailAddress == EmailAddress).ToList();
+            return result.Count > 0;
+        }
+
+
+
+        
+    }
+}
---
+++ OrderRequestWeb/OrderRequestWeb/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLogicLayer;
+using OrderRequestWeb.Models;
+
+
+namespace OrderRequestWeb.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //
+        // GET: /Customer/
+        private OrderRequestWeb.Models.CustomerModel.CustomerRegistrationInputModel model = new Models.CustomerModel.CustomerRegistrationInputModel();
+        private BusinessLogicLayer.CustomerService CustomerService = new CustomerService();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            
+            return View();
+        }
+        [Authorize]
+        public JsonResult IsEmailExist(string EmailAddress)
+        {
+           
+            if (CustomerService.IsEmailExixt(EmailAddress))
+            {
+                return Json("Sorry, this name already exists", JsonRequestBehavior.AllowGet);
+
+            }
+            else
+            {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+
+        }
+
+
+    }
+}
